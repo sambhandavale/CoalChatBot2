d20d95b353872ab0e64f345a54cb111897d3dc7b
sambhandavale <125118342+sambhandavale@users.noreply.github.com>
2023-09-13 23:08:30 +0530

    Update Home2.js

--- JS/Home2.js
+++ JS/Home2.js
@@ -15,5 +15,5 @@ window.addEventListener('scroll', function() {
     coal.style.marginLeft = value * 1.25  + "px";
     mountains_behind.style.top = value * 0.5  + "px";
     mountains_front.style.top = value * 0.5  + "px";
-    text.style.marginRight = value * 2  + "px";
+    text.style.marginRight = value *2  + "px";
 });
